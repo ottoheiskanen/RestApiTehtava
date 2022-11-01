502b49bde55b0b766e7ac7572defdf493a32dacf
kahvimorso <ottoheiskanen@gmail.com>
2022-11-01 16:27:30 +0200

    added db

---
+++ config/db.js
@@ -0,0 +1,22 @@
+require("dotenv").config() //refer to .env file
+const mysql = require("mysql2")
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+})
+
+// TESTING CODE
+let sql = "SELECT * FROM posts"
+
+pool.execute(sql, function (err, result) {
+    if (err) throw err
+
+    result.forEach((res) => {
+        console.log(res.title)
+    })
+})
+
+module.exports = pool.promise()
